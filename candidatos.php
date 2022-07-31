d0a8e485bed7f38ffd7fb23db1b1f9ab68feb7a6
joaopedro489 <joao.pedro.489@hotmail.com>
2022-07-31 19:25:21 -0300

    try again

--- candidatos.php
+++ candidatos.php
@@ -1,99 +1,100 @@
 <?php
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+echo json_encode($arr);
+// // Gera um objeto com os dados dos vereadores, prefeitos e seus respectivos vice-prefeitos.
+// $candidatos = [];
+// $vereadoresResponse = getVereadores();
+// $prefeitosResponse = getPrefeitos();
+// array_push($candidatos, $vereadoresResponse);
+// array_push($candidatos, $prefeitosResponse);
+// print_r($candidatos);
+// $var = json_encode($candidatos);
+// // Retorna um json contendo os dados dos candidatos
+// header('Content-Type: application/json; charset=utf-8');
+// echo $var;
 
-// Gera um objeto com os dados dos vereadores, prefeitos e seus respectivos vice-prefeitos.
-$candidatos = [];
-$vereadoresResponse = getVereadores();
-$prefeitosResponse = getPrefeitos();
-array_push($candidatos, $vereadoresResponse);
-array_push($candidatos, $prefeitosResponse);
-print_r($candidatos);
-$var = json_encode($candidatos);
-// Retorna um json contendo os dados dos candidatos
-header('Content-Type: application/json; charset=utf-8');
-echo $var;
+// /**
+//  * Retorna um objeto contendo o nome, título e número de todos os vereadores
+//  * no banco de dados. No objeto também é inserido o nome do títlulo (vereador)
+//  * e o número de digitos no número de cada candidadto.
+//  * 
+//  * @return dados dos vereadores.
+//  */
+// function getVereadores() {
 
-/**
- * Retorna um objeto contendo o nome, título e número de todos os vereadores
- * no banco de dados. No objeto também é inserido o nome do títlulo (vereador)
- * e o número de digitos no número de cada candidadto.
- * 
- * @return dados dos vereadores.
- */
-function getVereadores() {
+//   // Conecta com o banco de dados
+//   include ('connect.php'); 
 
-  // Conecta com o banco de dados
-  include ('connect.php'); 
+//   // Executa a query para obter vereadores
+//   $vereadores_query = $conn->query("
+//   SELECT numero, nome, partido, foto 
+//   FROM Candidato
+//   WHERE titulo = 'vereador';
+//   ");
 
-  // Executa a query para obter vereadores
-  $vereadores_query = $conn->query("
-  SELECT numero, nome, partido, foto 
-  FROM Candidato
-  WHERE titulo = 'vereador';
-  ");
+//   // Retorna erro em caso de falha na query
+//   if(!$vereadores_query) {
+//     http_response_code(500);
+//     die("Erro ao obter vereadores.");
+//   }
+  
+//   $row = $vereadores_query->fetch_assoc();
+  
+//   $vereadores['titulo'] = 'vereador';
+//   $vereadores['numeros'] = strlen($row['numero']);
+  
+//   do {
+//     $vereadores['candidatos'][$row['numero']] = $row;
+//     unset($vereadores['candidatos'][$row['numero']]['numero']);
+//     $row = $vereadores_query->fetch_assoc();
+//   } while ( $row != null);
+  
+//   return $vereadores;
+// }
 
-  // Retorna erro em caso de falha na query
-  if(!$vereadores_query) {
-    http_response_code(500);
-    die("Erro ao obter vereadores.");
-  }
+// /**
+//  * Retorna um objeto contendo o nome, título e número de todos os prefeitos
+//  * e seus respectivos vice-prefeitos no banco de dados.
+//  * No objeto também é inserido o nome do títlulo (prefeito)
+//  * e o número de digitos no número de cada candidadto.
+//  * 
+//  * @return dados dos prefeitos e vice-prefeitos.
+//  */
+// function getPrefeitos() {
   
-  $row = $vereadores_query->fetch_assoc();
+//   // Conecta com o banco de dados
+//   include ('connect.php');
   
-  $vereadores['titulo'] = 'vereador';
-  $vereadores['numeros'] = strlen($row['numero']);
+//   // Executa a query para obter prefeitos e vice-prefeitos
+//   $prefeitos_query = $conn->query("
+//   SELECT *
+//   FROM Candidato INNER JOIN Vice USING (numero)
+//   WHERE titulo = 'prefeito';
+//   ");
   
-  do {
-    $vereadores['candidatos'][$row['numero']] = $row;
-    unset($vereadores['candidatos'][$row['numero']]['numero']);
-    $row = $vereadores_query->fetch_assoc();
-  } while ( $row != null);
+//   // Retorna erro em caso de falha na query
+//   if(!$prefeitos_query) {
+//     http_response_code(500);
+//     die("Erro ao obter prefeitos.");
+//   }
   
-  return $vereadores;
-}
+//   $row = $prefeitos_query->fetch_row();
+  
+//   $prefeitos['titulo'] = 'prefeito';
+//   $prefeitos['numeros'] = strlen($row[0]);
+  
+  
+//   do {
+//     $prefeitos['candidatos'][$row[0]]['nome'] = $row[1];
+//     $prefeitos['candidatos'][$row[0]]['partido'] = $row[3];
+//     $prefeitos['candidatos'][$row[0]]['foto'] = $row[4];
+//     $prefeitos['candidatos'][$row[0]]['vice']['nome'] = $row[6];
+//     $prefeitos['candidatos'][$row[0]]['vice']['partido'] = $row[7];
+//     $prefeitos['candidatos'][$row[0]]['vice']['foto'] = $row[8];
+//     $row = $prefeitos_query->fetch_row();
+//   } while ( $row != null);
+  
+//   return $prefeitos;
+// }
 
-/**
- * Retorna um objeto contendo o nome, título e número de todos os prefeitos
- * e seus respectivos vice-prefeitos no banco de dados.
- * No objeto também é inserido o nome do títlulo (prefeito)
- * e o número de digitos no número de cada candidadto.
- * 
- * @return dados dos prefeitos e vice-prefeitos.
- */
-function getPrefeitos() {
-  
-  // Conecta com o banco de dados
-  include ('connect.php');
-  
-  // Executa a query para obter prefeitos e vice-prefeitos
-  $prefeitos_query = $conn->query("
-  SELECT *
-  FROM Candidato INNER JOIN Vice USING (numero)
-  WHERE titulo = 'prefeito';
-  ");
-  
-  // Retorna erro em caso de falha na query
-  if(!$prefeitos_query) {
-    http_response_code(500);
-    die("Erro ao obter prefeitos.");
-  }
-  
-  $row = $prefeitos_query->fetch_row();
-  
-  $prefeitos['titulo'] = 'prefeito';
-  $prefeitos['numeros'] = strlen($row[0]);
-  
-  
-  do {
-    $prefeitos['candidatos'][$row[0]]['nome'] = $row[1];
-    $prefeitos['candidatos'][$row[0]]['partido'] = $row[3];
-    $prefeitos['candidatos'][$row[0]]['foto'] = $row[4];
-    $prefeitos['candidatos'][$row[0]]['vice']['nome'] = $row[6];
-    $prefeitos['candidatos'][$row[0]]['vice']['partido'] = $row[7];
-    $prefeitos['candidatos'][$row[0]]['vice']['foto'] = $row[8];
-    $row = $prefeitos_query->fetch_row();
-  } while ( $row != null);
-  
-  return $prefeitos;
-}
-
-?>
+// ?>

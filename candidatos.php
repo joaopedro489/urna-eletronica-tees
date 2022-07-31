0b43b063b32bc4cdf2a69b5cdfea5dc8ed8361cc
joaopedro489 <joao.pedro.489@hotmail.com>
2022-07-31 19:28:33 -0300

    try again

--- candidatos.php
+++ candidatos.php
@@ -4,9 +4,9 @@ $candidatos = [];
 $vereadoresResponse = getVereadores();
 
 $prefeitosResponse = getPrefeitos();
-echo json_encode($prefeitosResponse);
 array_push($candidatos, $vereadoresResponse);
 array_push($candidatos, $prefeitosResponse);
+print_r($candidatos);
 //$var = json_encode($candidatos);
 // Retorna um json contendo os dados dos candidatos
 header('Content-Type: application/json; charset=utf-8');
